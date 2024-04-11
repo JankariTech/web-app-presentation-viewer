87244be69a305bea484872e0ce4504c7eac22dfe
Saw-jan <saw.jan.grg3e@gmail.com>
2024-04-11 17:45:08 +0545

    feat: use images from same directory level

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     :class="{ 'dark-mode': isDarkMode }"
   >
     <div class="reveal">
-      <div class="slides">
+      <div ref="slideContainer" class="slides">
         <section
           :data-markdown="url"
           :data-separator="dataSeparator"
@@ -17,10 +17,13 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
-
-import { useThemeStore } from '@ownclouders/web-pkg'
-
+import { onMounted, ref, unref, watch } from 'vue'
+import {
+  useThemeStore,
+  useAppDefaults,
+  useAppFileHandling,
+  useClientService
+} from '@ownclouders/web-pkg'
 import Reveal from 'reveal.js'
 import RevealMarkdown from 'reveal.js/plugin/markdown/markdown'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
@@ -29,10 +32,23 @@ import 'reveal.js/dist/reveal.css'
 import 'reveal.js/plugin/highlight/monokai.css'
 import 'reveal.js/dist/theme/white.css'
 
+import { id as appId } from '../public/manifest.json'
+
+const themeStore = useThemeStore()
+const { loadFolderForFileContext, currentFileContext, activeFiles } = useAppDefaults({
+  applicationId: appId
+})
+const { getUrlForResource, revokeUrl } = useAppFileHandling({
+  clientService: useClientService
+})
+
+const isDarkMode = ref(themeStore.currentTheme.isDark)
+const slideContainer = ref<HTMLElement | undefined>()
+
 const dataSeparator = '\r?\n---\r?\n'
 const dataSeparatorVertical = '\r?\n--\r?\n'
-const themeStore = useThemeStore()
-const isDarkMode = ref(themeStore.currentTheme.isDark)
+const images = {}
+
 let reveal: Reveal.Api
 
 defineProps({
@@ -49,7 +65,10 @@ watch(
   }
 )
 
-onMounted(() => {
+onMounted(async () => {
+  await loadFolderForFileContext(unref(currentFileContext))
+  await parseImagesUrl()
+
   reveal = new Reveal({
     plugins: [RevealMarkdown, RevealHighlight]
   })
@@ -61,7 +80,34 @@ onMounted(() => {
     center: true,
     controlsLayout: 'edges'
   })
+
+  reveal.on('ready', () => {
+    const imgElements = unref(slideContainer).getElementsByTagName('img')
+    updateImageUrls(imgElements)
+  })
 })
+
+// METHODS
+function updateImageUrls(imgElements: HTMLCollectionOf<HTMLImageElement>) {
+  for (const el of imgElements) {
+    const src = el.src.split('/').pop()
+    el.src = images[src]
+  }
+}
+async function parseImagesUrl() {
+  for (const file of unref(activeFiles)) {
+    // TODO: use image mimetypes
+    if (file.extension === 'png') {
+      const url = await getUrlForResource(unref(currentFileContext).space, file)
+      images[file.name] = await getBlobUrl(url)
+    }
+  }
+}
+async function getBlobUrl(url: string) {
+  const data = await fetch(url).then(async (res) => await res.blob())
+  const blob = new Blob([data], { type: data.type })
+  return URL.createObjectURL(blob)
+}
 </script>
 
 <style lang="scss">
