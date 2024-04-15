11c0c83e41a006e8844fdf08ebb8cd4370109771
Saw-jan <saw.jan.grg3e@gmail.com>
2024-04-15 11:30:51 +0545

    refactor: filter media files

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,15 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, unref, watch } from 'vue'
+import { computed, onMounted, ref, unref, watch } from 'vue'
 import {
   useThemeStore,
   useAppDefaults,
   useAppFileHandling,
-  useClientService
+  useClientService,
+  useAppsStore
 } from '@ownclouders/web-pkg'
+import { Resource } from '@ownclouders/web-client/src'
 import Reveal from 'reveal.js'
 import RevealMarkdown from 'reveal.js/plugin/markdown/markdown'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
@@ -32,6 +34,7 @@ import 'reveal.js/dist/reveal.css'
 import 'reveal.js/plugin/highlight/monokai.css'
 import 'reveal.js/dist/theme/white.css'
 
+import { getMediaMimeTypes } from './helpers/mediaMimeTypes'
 import { id as appId } from '../public/manifest.json'
 
 const themeStore = useThemeStore()
@@ -41,13 +44,14 @@ const { loadFolderForFileContext, currentFileContext, activeFiles } = useAppDefa
 const { getUrlForResource, revokeUrl } = useAppFileHandling({
   clientService: useClientService
 })
+const appsStore = useAppsStore()
 
 const isDarkMode = ref(themeStore.currentTheme.isDark)
 const slideContainer = ref<HTMLElement | undefined>()
+const imageUrls = ref()
 
 const dataSeparator = '\r?\n---\r?\n'
 const dataSeparatorVertical = '\r?\n--\r?\n'
-const images = {}
 
 let reveal: Reveal.Api
 
@@ -87,21 +91,38 @@ onMounted(async () => {
   })
 })
 
+// COMPUTED
+const mediaMimeTypes = computed(() =>
+  getMediaMimeTypes(appsStore.externalAppConfig['preview']?.mimeTypes)
+)
+const mediaFiles = computed<Resource[]>(() => {
+  if (!unref(activeFiles)) {
+    return []
+  }
+
+  return unref(activeFiles).filter((file: Resource) => {
+    return unref(mediaMimeTypes).includes(file.mimeType?.toLowerCase())
+  })
+})
+
 // METHODS
 function updateImageUrls(imgElements: HTMLCollectionOf<HTMLImageElement>) {
+  const imgUrls = unref(imageUrls)
   for (const el of imgElements) {
     const src = el.src.split('/').pop()
-    el.src = images[src]
+    el.src = imgUrls[src]
   }
 }
 async function parseImagesUrl() {
-  for (const file of unref(activeFiles)) {
+  const images = {}
+  for (const file of unref(mediaFiles)) {
     // TODO: use image mimetypes
     if (file.extension === 'png') {
       const url = await getUrlForResource(unref(currentFileContext).space, file)
       images[file.name] = await getBlobUrl(url)
     }
   }
+  imageUrls.value = images
 }
 async function getBlobUrl(url: string) {
   const data = await fetch(url).then(async (res) => await res.blob())
