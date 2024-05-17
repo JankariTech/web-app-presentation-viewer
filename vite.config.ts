ad6d75c1c09354832f8500a4e3f664db5d22e7ab
Saw-jan <saw.jan.grg3e@gmail.com>
2024-05-17 09:56:24 +0545

    build: configure local dev setup for latest ocis

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
 import { id } from './public/manifest.json'
 
-// TODO: make APPS_LOADING default after oCIS 5.1 is released
-const base =
-  process.env.APPS_LOADING === 'true' ? '/assets/apps' : 'https://host.docker.internal:3000'
-
 export default defineConfig({
-  base: `${base}/${id}/`,
+  base: `/assets/apps/${id}/`,
   build: {
     rollupOptions: {
       output: {
