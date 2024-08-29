f5da88ebf84fa84f98801f3cd577dbd9a5120b39
Saw-jan <saw.jan.grg3e@gmail.com>
2024-08-29 12:27:26 +0545

    fix: local dev setup for WEB_ASSET

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
 import { id } from './public/manifest.json'
 
-// TODO: make APPS_LOADING default after oCIS 5.1 is released
+// TODO: make APPS_LOADING default after oCIS 6.0 is released
 const base =
   process.env.APPS_LOADING === 'true' ? '/assets/apps' : 'https://host.docker.internal:3000'
 
